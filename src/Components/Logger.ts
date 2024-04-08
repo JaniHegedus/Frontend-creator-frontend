fd3d41f089da9341a238695473c9a8ef213e16be
Hegedüs János <hegedusjanos2002@gmail.com>
2024-04-08 05:23:28 +0200

    Updated Logger Looks

--- src/Components/Logger.ts
+++ src/Components/Logger.ts
@@ -1,3 +1,13 @@
-import pino from 'pino';
-const logger = pino();
+import pino, {TransportTargetOptions} from 'pino';
+const transport: TransportTargetOptions = {
+    target: 'pino-pretty',
+    options: {
+        colorize: true,
+        translateTime: "UTC:yyyy-mm-dd'T'HH:MM:ss",
+        ignore: 'pid,hostname',
+    },
+}
+const logger = pino({
+    transport
+});
 export default logger
